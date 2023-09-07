ddf132d66124e79ce8d57c490d6b48dda2bebe18
alberand <andrey.albershteyn@gmail.com>
2023-09-07 10:49:33 +0200

    Common user-modules, minor fixes, clean-up

--- flake.nix
+++ flake.nix
@@ -16,11 +16,122 @@
     };
     # default kernel if no custom kernel were specified
     root = builtins.toString ./.;
+    modules = [
+      ({config, pkgs, ...}: {
+          programs.xfstests = {
+            enable = true;
+            src = pkgs.fetchFromGitHub {
+              owner = "alberand";
+              repo = "xfstests";
+              rev = "068d7af36369c7c3da7d165c50b378e7b7ce46fd";
+              sha256 = "sha256-okVvdUG7ixDm7awquBvLQmN4qGma4DFM8vSJ/4VJoL0=";
+            };
+            autoshutdown = false;
+            testconfig = ./xfstests.config;
+            arguments = "-s xfs_4k generic/110";
+          };
+
+          # Let's also include specific version of xfsprogs
+          nixpkgs.overlays = [
+            (self: super: {
+              xfsprogs = super.xfsprogs.overrideAttrs ({
+                version = "git";
+                src = pkgs.fetchFromGitHub {
+                  owner = "alberand";
+                  repo = "xfsprogs";
+                  rev = "12ee5324c60e5394b4d8a2b58726a4aadfaf0ac9";
+                  sha256 = "sha256-pWfBo6MHmiCTB172NFLdD/oNEih0ntLrM6NIQIVXD80=";
+                };
+              });
+            })
+          ];
+
+          boot.kernelPackages = (pkgs.linuxPackagesFor (pkgs.callPackage pkgs.linuxManualConfig {
+              inherit (pkgs) stdenv;
+              version = "6.5.0-rc4";
+              # ignoreConfigErrors = true;
+              allowImportFromDerivation = true;
+              src = pkgs.fetchFromGitHub {
+                owner = "alberand";
+                repo = "linux";
+                rev = "17a940a32fd02da8e26ae984f0da9e73c1c163ab";
+                sha256 = "sha256-3iyomhZPVv/EoBdMyrtfB74FOFfcg+w36OWNtMJKHiU=";
+              };
+              configfile = /home/alberand/Projects/xfs-verity-v3/.config;
+              config = {
+                CONFIG_AUTOFS4_FS = "y";
+                CONFIG_VIRTIO_BLK = "y";
+                CONFIG_VIRTIO_PCI = "y";
+                CONFIG_VIRTIO_NET = "y";
+                CONFIG_EXT4_FS = "y";
+                CONFIG_NET_9P_VIRTIO = "y";
+                CONFIG_9P_FS = "y";
+                CONFIG_BLK_DEV = "y";
+                CONFIG_PCI = "y";
+                CONFIG_NETDEVICES = "y";
+                CONFIG_NET_CORE = "y";
+                CONFIG_INET = "y";
+                CONFIG_NETWORK_FILESYSTEMS = "y";
+                CONFIG_SERIAL_8250_CONSOLE = "y";
+                CONFIG_SERIAL_8250 = "y";
+                CONFIG_OVERLAY_FS = "y";
+                CONFIG_DEVTMPFS = "y";
+                CONFIG_CGROUPS = "y";
+                CONFIG_SIGNALFD = "y";
+                CONFIG_TIMERFD = "y";
+                CONFIG_EPOLL = "y";
+                CONFIG_SYSFS = "y";
+                CONFIG_PROC_FS = "y";
+                CONFIG_FHANDLE = "y";
+                CONFIG_CRYPTO_USER_API_HASH = "y";
+                CONFIG_CRYPTO_HMAC = "y";
+                CONFIG_CRYPTO_SHA256 = "y";
+                CONFIG_DMIID = "y";
+                CONFIG_TMPFS_POSIX_ACL = "y";
+                CONFIG_TMPFS_XATTR = "y";
+                CONFIG_SECCOMP = "y";
+                CONFIG_TMPFS = "y";
+                CONFIG_BLK_DEV_INITRD = "y";
+                CONFIG_MODULES = "y";
+                CONFIG_BINFMT_ELF = "y";
+                CONFIG_UNIX = "y";
+                CONFIG_INOTIFY_USER = "y";
+                CONFIG_NET = "y";
+              };
+          }));
+          #boot.kernelPackages = let
+          #  linux-custom = { fetchurl, buildLinux, ... } @ args:
+          #  buildLinux (args // rec {
+          #    version = "6.4.0-rc2";
+          #    modDirVersion = version;
+
+          #    src = pkgs.fetchFromGitHub {
+          #      owner = "alberand";
+          #      repo = "linux";
+          #      rev = "17a940a32fd02da8e26ae984f0da9e73c1c163ab";
+          #      sha256 = "sha256-3iyomhZPVv/EoBdMyrtfB74FOFfcg+w36OWNtMJKHiU=";
+          #    };
+
+          #    extraConfig = ''
+          #      FS_VERITY y
+          #      FS_VERITY_BUILTIN_SIGNATURES y
+          #      XFS_DEBUG y
+          #      XFS_ASSERT_FATAL y
+          #      XFS_RT n
+          #      XFS_ONLINE_SCRUB n
+          #    '';
+          #  } // (args.argsOverride or {}));
+          #  kernel = pkgs.callPackage linux-custom {};
+          #in
+          #  pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor kernel);
+          })
+    ];
   in rec {
     lib = {
       mkVM = {
         pkgs,
         sharepoint,
+        qemu-options ? [],
         user-modules ? []
       }: nixos-generators.nixosGenerate {
         system = "x86_64-linux";
@@ -30,7 +141,7 @@
             virtualisation = {
               diskSize = 20000; # MB
               # Store the image in sharepoint instead of pwd
-              diskImage = "${sharepoint}/vm.qcow2";
+              diskImage = "${sharepoint}/test-node.qcow2";
               memorySize = 4096; # MB
               cores = 4;
               writableStoreUseTmpfs = false;
@@ -39,10 +150,10 @@
               graphics = false;
 
               qemu = {
-                networkingOptions = [
+                options = [
                   "-device e1000,netdev=network0,mac=00:00:00:00:00:00"
                   "-netdev tap,id=network0,ifname=tap0,script=no,downscript=no"
-                ];
+                ] ++ qemu-options;
               };
 
               sharedDirectories = {
@@ -78,16 +189,17 @@
       mkVmTest = {
         pkgs,
         sharepoint,
+        qemu-options ? [],
         user-modules ? []
       }:
       builtins.getAttr "vmtest" rec {
         nixos = lib.mkVM {
-          inherit pkgs sharepoint user-modules;
+          inherit pkgs sharepoint qemu-options user-modules;
         };
 
         vmtest = pkgs.writeScriptBin "vmtest"
         ((builtins.readFile ./run.sh) + ''
-            ${nixos}/bin/run-vm-vm
+            ${nixos}/bin/run-test-node-vm
             echo "View results at $SHARE_DIR/results"
         '');
       };
@@ -96,13 +208,14 @@
         pkgs,
         root,
         sharepoint ? "/tmp/vmtest",
+        qemu-options ? [],
         user-modules ? [],
       }:
       builtins.getAttr "shell" rec {
         shell = pkgs.mkShell {
           packages = with pkgs; [
             (lib.mkVmTest {
-              inherit pkgs sharepoint user-modules;
+              inherit pkgs sharepoint qemu-options user-modules;
             })
           ];
 
@@ -148,7 +261,8 @@
           ];
 
           shellHook = ''
-            if [ ! -f ${root}/compile_commands.json ]; then
+            if [ ! -f ${root}/compile_commands.json ] &&
+                [ -f ${root}/scripts/clang-tools/gen_compile_commands.py ]; then
               ${root}/scripts/clang-tools/gen_compile_commands.py
             fi
           '';
@@ -159,180 +273,28 @@
 
     devShells.default = lib.mkLinuxShell {
       inherit pkgs root;
-      user-modules = [
-        ({config, pkgs, ...}: {
-
-          # Just a test user to check that our modules are enabled
-        users.users.hahaha-andrey = {
-          isNormalUser  = true;
-          description  = "hahaha-andrey user";
-        };
-
-        # Let's enable xfstests service
-          programs.xfstests = {
-            enable = true;
-            src = pkgs.fetchFromGitHub {
-              owner = "alberand";
-              repo = "xfstests";
-              rev = "2500effc2c77939343161304bf436dfd58c73735";
-              sha256 = "sha256-XxtFm7a+NRvv8xzaVMBWe+B0BtLZLoNGH6IlGbTu4NE=";
-            };
-            autoshutdown = false;
-            testconfig = ./xfstests.config;
-            arguments = "-s xfs_4k generic/001 generic/002";
-          };
-
-          # Let's also include specific version of xfsprogs
-          nixpkgs.overlays = [
-            (self: super: {
-              xfsprogs = super.xfsprogs.overrideAttrs ({
-                version = "git";
-                src = pkgs.fetchFromGitHub {
-                  owner = "alberand";
-                  repo = "xfsprogs";
-                  rev = "12ee5324c60e5394b4d8a2b58726a4aadfaf0ac9";
-                  sha256 = "sha256-pWfBo6MHmiCTB172NFLdD/oNEih0ntLrM6NIQIVXD80=";
-                };
-              });
-            })
-          ];
-
-          # Let's append real hardware to the QEMU run by "vmtest" command
-          virtualisation = {
-            qemu = {
-              networkingOptions = [
-                "-hdc /dev/sdc4 -hdd /dev/sdc5 -serial mon:stdio"
-              ];
-            };
-          };
-          # Let's specify kernel we want our VM to be built with
-          #boot.kernelPackages = let
-          #  linux-custom = { fetchurl, buildLinux, ... } @ args:
-          #  buildLinux (args // rec {
-          #    version = "6.4.0-rc3";
-          #    modDirVersion = version;
-
-          #    src = fetchurl {
-          #      url = "https://git.kernel.org/torvalds/t/linux-6.4-rc3.tar.gz";
-          #      sha256 = "sha256-xlN7KcrtykVG3W9DDbODKNKJehGCAQOr4R2uw3hfxoE=";
-          #    };
-
-          #    extraConfig = ''
-          #    '';
-          #  } // (args.argsOverride or {}));
-          #  kernel = pkgs.callPackage linux-custom {};
-          #in
-          #  pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor kernel);
-        })
+      sharepoint = "/tmp/sharepoint";
+      qemu-options = [
+        "-hdc /dev/sdd4 -hdd /dev/sdd5 -serial mon:stdio"
       ];
+      user-modules = modules;
     };
 
     packages = rec {
       default = vmtest;
+
       vmtest = lib.mkVmTest {
         inherit pkgs;
         sharepoint = "/tmp/vmtest";
-        user-modules = [
-          ({config, pkgs, ...}: {
-          programs.xfstests = {
-            enable = true;
-            src = pkgs.fetchFromGitHub {
-              owner = "alberand";
-              repo = "xfstests";
-              rev = "068d7af36369c7c3da7d165c50b378e7b7ce46fd";
-              sha256 = "sha256-okVvdUG7ixDm7awquBvLQmN4qGma4DFM8vSJ/4VJoL0=";
-            };
-            autoshutdown = false;
-            testconfig = ./xfstests.config;
-            arguments = "-s xfs_4k generic/110";
-          };
-
-          # Let's also include specific version of xfsprogs
-          nixpkgs.overlays = [
-            (self: super: {
-              xfsprogs = super.xfsprogs.overrideAttrs ({
-                version = "git";
-                src = pkgs.fetchFromGitHub {
-                  owner = "alberand";
-                  repo = "xfsprogs";
-                  rev = "12ee5324c60e5394b4d8a2b58726a4aadfaf0ac9";
-                  sha256 = "sha256-pWfBo6MHmiCTB172NFLdD/oNEih0ntLrM6NIQIVXD80=";
-                };
-              });
-            })
-          ];
-
-          # Let's append real hardware to the QEMU run by "vmtest" command
-          virtualisation = {
-            qemu = {
-              networkingOptions = [
-                "-hdc /dev/sdc4 -hdd /dev/sdc5 -serial mon:stdio"
-              ];
-            };
-          };
-          })
+        qemu-options = [
+          "-hdc /dev/sdd4 -hdd /dev/sdd5 -serial mon:stdio"
         ];
+        user-modules = modules;
       };
+
       iso = lib.mkIso {
         inherit pkgs;
-        user-modules = [
-          ({config, pkgs, ...}: {
-          programs.xfstests = {
-            enable = true;
-            src = pkgs.fetchFromGitHub {
-              owner = "alberand";
-              repo = "xfstests";
-              rev = "068d7af36369c7c3da7d165c50b378e7b7ce46fd";
-              sha256 = "sha256-okVvdUG7ixDm7awquBvLQmN4qGma4DFM8vSJ/4VJoL0=";
-            };
-            autoshutdown = false;
-            testconfig = ./xfstests.config;
-            arguments = "-g auto -g verity";
-          };
-
-          # Let's also include specific version of xfsprogs
-          nixpkgs.overlays = [
-            (self: super: {
-              xfsprogs = super.xfsprogs.overrideAttrs ({
-                version = "git";
-                src = pkgs.fetchFromGitHub {
-                  owner = "alberand";
-                  repo = "xfsprogs";
-                  rev = "12ee5324c60e5394b4d8a2b58726a4aadfaf0ac9";
-                  sha256 = "sha256-pWfBo6MHmiCTB172NFLdD/oNEih0ntLrM6NIQIVXD80=";
-                };
-              });
-            })
-          ];
-
-          boot.kernelPackages = let
-            linux-custom = { fetchurl, buildLinux, ... } @ args:
-            buildLinux (args // rec {
-              version = "6.4.0-rc2";
-              modDirVersion = version;
-
-              src = pkgs.fetchFromGitHub {
-                owner = "alberand";
-                repo = "linux";
-                rev = "17a940a32fd02da8e26ae984f0da9e73c1c163ab";
-                sha256 = "sha256-3iyomhZPVv/EoBdMyrtfB74FOFfcg+w36OWNtMJKHiU=";
-              };
-
-              extraConfig = ''
-                FS_VERITY y
-                FS_VERITY_BUILTIN_SIGNATURES y
-                XFS_DEBUG y
-                XFS_ASSERT_FATAL y
-                XFS_RT n
-                XFS_ONLINE_SCRUB n
-              '';
-            } // (args.argsOverride or {}));
-            kernel = pkgs.callPackage linux-custom {};
-          in
-            pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor kernel);
-
-        }) # module end
-        ];
+        user-modules = modules;
       };
     };
 
