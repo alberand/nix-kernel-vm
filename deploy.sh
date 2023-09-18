ad971f82b1ee7663f6b7d1b6f8f0a545a40594d9
alberand <andrey.albershteyn@gmail.com>
2023-09-18 12:03:18 +0200

    deploy: add initial ISO deploy script

---
+++ deploy.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env sh
+
+TEST_HOST=$TEST_HOST
+SYSNAME="${1:-defaultname}"
+TEST_ISO="$2"
+PREFIX="aalbersh"
+TEST_SYSTEM_XML="$PREFIX-$SYSNAME.xml"
+
+function system_xml() {
+	cp template.xml $TEST_SYSTEM_XML
+
+	xmlstarlet edit --inplace \
+		--update "/domain/name" \
+		--value "$PREFIX-$SYSNAME" $TEST_SYSTEM_XML
+
+	xmlstarlet edit --inplace \
+		--update "/domain/title" \
+		--value "Running xfstests" $TEST_SYSTEM_XML
+
+	xmlstarlet edit --inplace \
+		--update "/domain/devices/disk[@device='cdrom']/source/@file" \
+		--value "/tmp/$SYSNAME.iso" $TEST_SYSTEM_XML
+
+	xmlstarlet edit --inplace \
+		--update "/domain/devices/disk[@device='disk'][1]/source/@volume" \
+		--value "$PREFIX-$SYSNAME-test" $TEST_SYSTEM_XML
+
+	xmlstarlet edit --inplace \
+		--update "/domain/devices/disk[@device='disk'][2]/source/@volume" \
+		--value "$PREFIX-$SYSNAME-scratch" $TEST_SYSTEM_XML
+}
+
+function volume_xml() {
+	name="$PREFIX-$SYSNAME-$1.xml"
+	
+	cp volume.xml $name
+
+	xmlstarlet edit --inplace \
+		--update "/volume/name" \
+		--value "$PREFIX-$SYSNAME-$1" $name
+
+	xmlstarlet edit --inplace \
+		--update "/volume/target/path" \
+		--value "/var/lib/libvirt/images/$PREFIX-$SYSNAME-$1.img" $name
+}
+
+if [[ -z "$TEST_HOST" ]]; then
+    echo '$TEST_HOST is not defined' 1>&2
+    exit 1
+fi
+
+system_xml
+volume_xml "test"
+volume_xml "scratch"
+
+set -e
+
+rsync --compress --zc=lz4 -a -P \
+	$TEST_SYSTEM_XML \
+	$TEST_HOST:/tmp/$SYSNAME.xml
+rsync --compress --zc=lz4 -a -P \
+	$PREFIX-$SYSNAME-test.xml \
+	$TEST_HOST:/tmp/$PREFIX-$SYSNAME-test.xml
+rsync --compress --zc=lz4 -a -P \
+	$PREFIX-$SYSNAME-scratch.xml \
+	$TEST_HOST:/tmp/$PREFIX-$SYSNAME-scratch.xml
+rsync --compress --zc=lz4 -a -P \
+	$TEST_ISO \
+	$TEST_HOST:/tmp/$SYSNAME.iso
+
+ssh -t $TEST_HOST "sudo virsh vol-create --pool default /tmp/$PREFIX-$SYSNAME-test.xml"
+ssh -t $TEST_HOST "sudo virsh vol-create --pool default /tmp/$PREFIX-$SYSNAME-scratch.xml"
+ssh -t $TEST_HOST "sudo virsh create /tmp/$SYSNAME.xml"
