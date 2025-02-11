62979ba23eec04baf9b3036c9b95f735cfcbf3c7
Andrey Albershteyn <aalbersh@kernel.org>
2025-02-11 10:24:13 +0100

    Cleanup of kernel.nix

--- kernel.nix
+++ kernel.nix
@@ -3,49 +3,29 @@
   stdenv,
   ccacheStdenv,
   lib,
-  callPackage,
-  rustc,
-  cargo,
-  rust-bindgen,
-  buildPackages,
-  rustPlatform,
 }: {
   src,
   kconfig,
   version,
   modDirVersion,
-  nixpkgs,
-  kernelPatches ? [],
-}: let
-  kernel =
-    ((callPackage "${nixpkgs}/pkgs/os-specific/linux/kernel/manual-config.nix" {})
-      {
-        inherit src version modDirVersion lib;
-        configfile = kconfig;
-
-        allowImportFromDerivation = true;
-        stdenv = ccacheStdenv;
-      })
-    .overrideAttrs (old: {
-      nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.cpio];
-      dontStrip = true;
-        patches =
-          [
-              ./randstruct-provide-seed.patch
-          ];
-      preConfigure = ''
-        export CCACHE_MAXSIZE=5G
-        export CCACHE_DIR=/var/cache/ccache/
-        export CCACHE_SLOPPINESS=random_seed
-        export KBUILD_BUILD_TIMESTAMP=""
-      '';
-    });
-
-  kernelPassthru = {
-    inherit (kconfig) structuredConfig;
-    inherit modDirVersion;
+}:
+(pkgs.linuxManualConfig
+  {
+    inherit src version modDirVersion lib;
     configfile = kconfig;
-    passthru = kernel.passthru // (removeAttrs kernelPassthru ["passthru"]);
-  };
-in
-  lib.extendDerivation true kernelPassthru kernel
+    allowImportFromDerivation = true;
+    stdenv = ccacheStdenv;
+  })
+.overrideAttrs (old: {
+  nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.cpio];
+  dontStrip = true;
+  patches = [
+    ./randstruct-provide-seed.patch
+  ];
+  preConfigure = ''
+    export CCACHE_MAXSIZE=5G
+    export CCACHE_DIR=/var/cache/ccache/
+    export CCACHE_SLOPPINESS=random_seed
+    export KBUILD_BUILD_TIMESTAMP=""
+  '';
+})
